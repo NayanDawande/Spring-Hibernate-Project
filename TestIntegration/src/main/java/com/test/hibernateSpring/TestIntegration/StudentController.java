1b09bccfcdea51cd2ccdcea5590b64b713a4ea1a
Nayan <Nayan@DESKTOP-PKM9AA4>
2022-12-26 12:14:12 +0530

    Third Commit by Nayan

--- TestIntegration/src/main/java/com/test/hibernateSpring/TestIntegration/StudentController.java
+++ TestIntegration/src/main/java/com/test/hibernateSpring/TestIntegration/StudentController.java
@@ -1,7 +1,10 @@
 package com.test.hibernateSpring.TestIntegration;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +16,23 @@ public class StudentController {
 	SessionFactory sessionFactory;
 	
 	@RequestMapping("/SingleRecord")
-	public Student showAllData() {
+	public Student showSingleData() {
 		Session session = sessionFactory.openSession();		
 		Student student = session.load(Student.class, 101);
 		System.out.println(student);
-		return student;
-		
-		
+		return student;	
 	}
+	
+	@RequestMapping("/AllRecord")
+	public List showAllData() {
+		Session session = sessionFactory.openSession();
+		String hql = "from Student";
+		Query query = session.createQuery(hql);
+		List result = query.list();		
+		System.out.println(result);
+		return result;
+	}
+	
 	
 	
 	
