5b0255d55546b74da7bef3766b86d77dbf366540
Nayan <Nayan@DESKTOP-PKM9AA4>
2022-12-26 13:10:42 +0530

    Fifth Commited by Nayan

--- TestIntegration/src/main/java/com/test/hibernateSpring/TestIntegration/StudentController.java
+++ TestIntegration/src/main/java/com/test/hibernateSpring/TestIntegration/StudentController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,11 +29,42 @@ public class StudentController {
 	@RequestMapping("/AllRecord")
 	public List showAllData() {
 		Session session = sessionFactory.openSession();
-		String hql = "from Student";
-		Query query = session.createQuery(hql);
+		Query query = session.createQuery("from Student");
 		List result = query.list();		
 		System.out.println(result);
 		return result;
+	}
+	
+	@RequestMapping("/SumId")
+	public List sumID() {
+		
+		Session session = sessionFactory.openSession();
+		Query query  = session.createQuery("select sum(id) from Student");
+		List<Integer> list = query.list();
+		System.out.println("Sum of ID's "+list.get(0));
+		return list;
+	}
+	
+	@RequestMapping("/CountData")
+	public List countID() {
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("select count(id) from Student");
+		List <Integer> list = query.list();
+		System.out.println("Count the Data "+list.get(0));			
+		return list;
+	}
+	
+	@PutMapping("/updateID/{id}/{name}")
+	public Student updateID(@PathVariable int id, @PathVariable String name) {
+		
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+	
+		Student student = new Student(id,name);	
+		session.update(student);
+		System.out.println("Updated Student ");
+		transaction.commit();
+		return student;
 	}
 	
 	
